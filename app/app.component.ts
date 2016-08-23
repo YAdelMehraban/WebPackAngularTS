613463bcab70d5e8c5370de2385ba6ff02e682cc
Yaser <adel.mehrban@gmail.com>
2016-08-23 21:56:42 +1000

    initial setup

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/// <reference path="../typings/index.d.ts" />
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>Asp.Net Core Angular 2 Starter Kit</h1>`
+})
+
+export class AppComponent {}
